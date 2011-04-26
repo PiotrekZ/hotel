2a361c45878a4e6ce2d2eefdfa613cacf80667af
Piotr Zakrzewski <pimpek.zak@wp.pl>
2011-04-26 20:34:24 +0200

    'first'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hotel::Application.config.secret_token = '60b707d44dbf15dd2fb8576ef20b9b147c1f9745ebffb9429c6e114af898e03f6994e70ec56e8e2bde27fdf12561be38ad715bc5b8d5969be9b23b73cb9bf64a'
